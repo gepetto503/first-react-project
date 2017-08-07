65954fbfae37545121e5f5ba849390072a55a17f
Brendan Hanna <gepetto503@gmail.com>
2017-08-07 16:48:12 -0700

    create initial files, link projects component to app component

---
+++ src/components/projects.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Projects extends Component {
+  render() {
+    return (
+      <div className="banana">
+        <h2>alisfdjldksjf</h2>
+      </div>
+    );
+  }
+}
+
+export default Projects;
